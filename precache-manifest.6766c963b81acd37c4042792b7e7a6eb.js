41fb1dfc1a8fd2e67f65cac257e08537af9ffb23
sem1234d <48213103+sem1234d@users.noreply.github.com>
2019-09-27 18:28:40 +0300

    Updates

---
+++ precache-manifest.6766c963b81acd37c4042792b7e7a6eb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd7e859a34bfa05fb954dd5a8ecac64",
+    "url": "/Quests_in_Pocket/index.html"
+  },
+  {
+    "revision": "c23d9db0fd8c594bd98f",
+    "url": "/Quests_in_Pocket/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "c23d9db0fd8c594bd98f",
+    "url": "/Quests_in_Pocket/static/js/2.81ee8bc4.chunk.js"
+  },
+  {
+    "revision": "30295f09171e2e8c5c69",
+    "url": "/Quests_in_Pocket/static/js/main.2ac6603f.chunk.js"
+  },
+  {
+    "revision": "b947d369c68a04a99f22",
+    "url": "/Quests_in_Pocket/static/js/runtime-main.6d123406.js"
+  },
+  {
+    "revision": "cc873e2de00d49030e6ee7036d0b1243",
+    "url": "/Quests_in_Pocket/static/media/forest.cc873e2d.png"
+  },
+  {
+    "revision": "e0ba97a3717d9d9c10b5d089ef4ac552",
+    "url": "/Quests_in_Pocket/static/media/map.e0ba97a3.png"
+  },
+  {
+    "revision": "230919a1db2fb1069b1c8bbcae0eae39",
+    "url": "/Quests_in_Pocket/static/media/one_1.230919a1.jpg"
+  },
+  {
+    "revision": "5e049246a7f18f3ef7cd8a59ea74294e",
+    "url": "/Quests_in_Pocket/static/media/one_colum.5e049246.jpg"
+  },
+  {
+    "revision": "f0d660588e21ad686036ece54e8a7042",
+    "url": "/Quests_in_Pocket/static/media/one_colum_2.f0d66058.jpg"
+  },
+  {
+    "revision": "11796e77ddac6ef1866df8fc80c8b71a",
+    "url": "/Quests_in_Pocket/static/media/one_colum_3.11796e77.jpg"
+  }
+]);
