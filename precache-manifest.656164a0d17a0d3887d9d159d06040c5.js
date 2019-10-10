e4fad88badb6cd27625314bc2c559971229851a4
sem1234d <48213103+sem1234d@users.noreply.github.com>
2019-10-10 15:48:56 +0300

    Updates

---
+++ precache-manifest.656164a0d17a0d3887d9d159d06040c5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc104ee7cfbe0d3a0f08c5d085e36a8c",
+    "url": "/Quests_in_Pocket/index.html"
+  },
+  {
+    "revision": "36cf0320e59e02fba8fa",
+    "url": "/Quests_in_Pocket/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "36cf0320e59e02fba8fa",
+    "url": "/Quests_in_Pocket/static/js/2.47a5d9f8.chunk.js"
+  },
+  {
+    "revision": "bf2530a02384a1762604",
+    "url": "/Quests_in_Pocket/static/js/main.b62da042.chunk.js"
+  },
+  {
+    "revision": "b947d369c68a04a99f22",
+    "url": "/Quests_in_Pocket/static/js/runtime-main.6d123406.js"
+  },
+  {
+    "revision": "2ea10a943ff8cd7ccc2a20a5d97801bd",
+    "url": "/Quests_in_Pocket/static/media/Design-story.2ea10a94.png"
+  },
+  {
+    "revision": "05917b64eb9eff057eb7c74b3bb2a141",
+    "url": "/Quests_in_Pocket/static/media/achiev-1.05917b64.png"
+  },
+  {
+    "revision": "cc873e2de00d49030e6ee7036d0b1243",
+    "url": "/Quests_in_Pocket/static/media/forest.cc873e2d.png"
+  },
+  {
+    "revision": "f3b163255450bbed312a2216b55a613f",
+    "url": "/Quests_in_Pocket/static/media/man.f3b16325.jpg"
+  },
+  {
+    "revision": "e0ba97a3717d9d9c10b5d089ef4ac552",
+    "url": "/Quests_in_Pocket/static/media/map.e0ba97a3.png"
+  },
+  {
+    "revision": "230919a1db2fb1069b1c8bbcae0eae39",
+    "url": "/Quests_in_Pocket/static/media/one_1.230919a1.jpg"
+  },
+  {
+    "revision": "5e049246a7f18f3ef7cd8a59ea74294e",
+    "url": "/Quests_in_Pocket/static/media/one_colum.5e049246.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Quests_in_Pocket/precache-manifest.09020ff6b13544183ca2fe2003117577.js"
+  "/Quests_in_Pocket/precache-manifest.656164a0d17a0d3887d9d159d06040c5.js"
 );
 
 self.addEventListener('message', (event) => {
