737629d7dbcd8d04cbafbc656f8d61a2e54e4ed1
sem1234d <48213103+sem1234d@users.noreply.github.com>
2019-10-16 17:50:36 +0300

    Updates

--- precache-manifest.469171c7c0bb94ab2f886d16a9baeb96.js
+++ precache-manifest.469171c7c0bb94ab2f886d16a9baeb96.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "88404f65b0022767af684fc6099c27b3",
+    "revision": "6cfc1a7eae37e954ac90ffeb6c83188a",
     "url": "/Quests_in_Pocket/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Quests_in_Pocket/static/js/2.42c1bd64.chunk.js"
   },
   {
-    "revision": "385d25b83202323a117c",
-    "url": "/Quests_in_Pocket/static/js/main.a2755b14.chunk.js"
+    "revision": "60f1911c468fc63a3e95",
+    "url": "/Quests_in_Pocket/static/js/main.ad4c0c03.chunk.js"
   },
   {
     "revision": "b947d369c68a04a99f22",
