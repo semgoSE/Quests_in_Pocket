9ed775614efc31b279624d81e425ea5e82fb063f
sem1234d <48213103+sem1234d@users.noreply.github.com>
2019-10-17 22:36:19 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Quests_in_Pocket/precache-manifest.f8f842975fcd7ca1342ae105eb3b570f.js"
+  "/Quests_in_Pocket/precache-manifest.87ca087f096e22f1a625f0288edc2c44.js"
 );
 
 self.addEventListener('message', (event) => {
