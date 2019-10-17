ea8bbcdc2858dec9788458f04a2cc92a92ab2048
sem1234d <48213103+sem1234d@users.noreply.github.com>
2019-10-17 22:18:30 +0300

    Updates

--- precache-manifest.19e63191abddba462fa792069ee87d70.js
+++ precache-manifest.19e63191abddba462fa792069ee87d70.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f5a1817e78af3c7d556283e5558f83bb",
+    "revision": "f5d8218fc8686c821bab21c76484c3ea",
     "url": "/Quests_in_Pocket/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Quests_in_Pocket/static/js/2.c2ebab13.chunk.js"
   },
   {
-    "revision": "c04ffb300e6c51dec513",
-    "url": "/Quests_in_Pocket/static/js/main.88db9697.chunk.js"
+    "revision": "024368d195b7eaaf1d91",
+    "url": "/Quests_in_Pocket/static/js/main.5e4215db.chunk.js"
   },
   {
     "revision": "b947d369c68a04a99f22",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Quests_in_Pocket/precache-manifest.0b646522a6b092c53587099325739d98.js"
+  "/Quests_in_Pocket/precache-manifest.19e63191abddba462fa792069ee87d70.js"
 );
 
 self.addEventListener('message', (event) => {
