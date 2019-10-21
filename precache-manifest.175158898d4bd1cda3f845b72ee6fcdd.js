208bb830d101ff66c2c81e628e52ff33a96346c7
sem1234d <48213103+sem1234d@users.noreply.github.com>
2019-10-21 22:14:49 +0300

    Updates

---
+++ precache-manifest.175158898d4bd1cda3f845b72ee6fcdd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "110ac911634c1d9e87d780612d86b2aa",
+    "url": "/Quests_in_Pocket/index.html"
+  },
+  {
+    "revision": "3c74426bb63155f21be2",
+    "url": "/Quests_in_Pocket/static/css/2.7214840c.chunk.css"
+  },
+  {
+    "revision": "3c74426bb63155f21be2",
+    "url": "/Quests_in_Pocket/static/js/2.084733e3.chunk.js"
+  },
+  {
+    "revision": "eb66c70d7031b9fd956c",
+    "url": "/Quests_in_Pocket/static/js/main.139d7709.chunk.js"
+  },
+  {
+    "revision": "483b88489446c161a2fe",
+    "url": "/Quests_in_Pocket/static/js/runtime-main.ef97b0e7.js"
+  },
+  {
+    "revision": "2ea10a943ff8cd7ccc2a20a5d97801bd",
+    "url": "/Quests_in_Pocket/static/media/Design-story.2ea10a94.png"
+  },
+  {
+    "revision": "05917b64eb9eff057eb7c74b3bb2a141",
+    "url": "/Quests_in_Pocket/static/media/achiev-1.05917b64.png"
+  },
+  {
+    "revision": "e0ba97a3717d9d9c10b5d089ef4ac552",
+    "url": "/Quests_in_Pocket/static/media/map.e0ba97a3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Quests_in_Pocket/precache-manifest.886975d67e8439ad33322aa93b4a7016.js"
+  "/Quests_in_Pocket/precache-manifest.175158898d4bd1cda3f845b72ee6fcdd.js"
 );
 
 self.addEventListener('message', (event) => {
