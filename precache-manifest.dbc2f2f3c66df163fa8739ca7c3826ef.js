52d694e5b99064a3c01a5197a1b50fd73ea62f10
mihago <mihago2004@gmail.com>
2019-11-21 13:22:41 +0300

    Updates

---
+++ precache-manifest.dbc2f2f3c66df163fa8739ca7c3826ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8fef89a6636e4460e1dd0bd5a59f2d",
+    "url": "/Quests_in_Pocket/index.html"
+  },
+  {
+    "revision": "a07b602b77357171965a",
+    "url": "/Quests_in_Pocket/static/css/2.266626f9.chunk.css"
+  },
+  {
+    "revision": "a07b602b77357171965a",
+    "url": "/Quests_in_Pocket/static/js/2.35f58a66.chunk.js"
+  },
+  {
+    "revision": "f2607c268e280e123482",
+    "url": "/Quests_in_Pocket/static/js/main.13eba48f.chunk.js"
+  },
+  {
+    "revision": "79e3e8f1da1531d41249",
+    "url": "/Quests_in_Pocket/static/js/runtime-main.d99eef2a.js"
+  },
+  {
+    "revision": "2ea10a943ff8cd7ccc2a20a5d97801bd",
+    "url": "/Quests_in_Pocket/static/media/Design-story.2ea10a94.png"
+  },
+  {
+    "revision": "05917b64eb9eff057eb7c74b3bb2a141",
+    "url": "/Quests_in_Pocket/static/media/achiev-1.05917b64.png"
+  },
+  {
+    "revision": "5aef6ad1f2a73e1e67bd09bf8e9d1155",
+    "url": "/Quests_in_Pocket/static/media/l.5aef6ad1.png"
+  },
+  {
+    "revision": "e0ba97a3717d9d9c10b5d089ef4ac552",
+    "url": "/Quests_in_Pocket/static/media/map.e0ba97a3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Quests_in_Pocket/precache-manifest.887b192a88c85b0539e2e546bd5321cd.js"
+  "/Quests_in_Pocket/precache-manifest.dbc2f2f3c66df163fa8739ca7c3826ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Quests_in_Pocket/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
